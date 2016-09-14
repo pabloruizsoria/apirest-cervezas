ca7f169c25e268e0557a254d6cc703a3c60272c7
PabloRuiz <pabloruizsoria@gmail.com>
2016-09-14 19:55:47 +0200

    Añado routers

--- app/server.js
+++ app/server.js
@@ -1,24 +1,17 @@
 var express = require('express') //llamamos a Express
-var app = express()
+var app = express()       
+var bodyParser = require('body-parser')        
 
-var port = process.env.PORT || 8080 // establecemos nuestro puerto
+var port = process.env.PORT || 8080  // establecemos nuestro puerto
 
-app.get('/', function(req, res) {
-  res.json({ mensaje: '¡Hola Mundo!' })
-})
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())            
 
-app.get('/cervezas', function(req, res) {
-  res.json({ mensaje: '¡A beber cerveza!' })
-})
+// nuestra ruta irá en http://localhost:8080/api
+// es bueno que haya un prefijo, sobre todo por el tema de versiones de la API
+var router = require('./routes')
+app.use('/api', router)
 
-app.post('/', function(req, res) {
-  res.json({ mensaje: 'Método post' })
-})
-
-app.del('/', function(req, res) {
-  res.json({ mensaje: 'Método delete' })
-})
-
-// iniciamos nuestro servidor
+//arrancamos el servidor
 app.listen(port)
 console.log('API escuchando en el puerto ' + port)
